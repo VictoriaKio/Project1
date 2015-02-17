f966a684c3eb5855a9dd844a3f096ee1516afe69
VictoriaKio <victoria.cj.kio@gmail.com>
2015-02-17 11:18:44 +0200

    Modulation of the Main Page on Project 1
    Formed the main page of Project 1, as a Wiki Page with the content for
    it in an object. I think the next step will be to create some tabs, the
    first tab, possibly a README page.

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+//Creating a Module - Test
+(function(){
 var app = angular.module("test", [ ]);
+
+app.controller('WikiText', function(){
+
+	this.wikicontent=wikicontent;
+
+});
+//Wiki content object
+//Need more objects, create an array of objects and use ng-repeat to display
+var wikicontent = {
+
+	title: 'Project Sandbox Angular',
+	description: 'These webpages are to study the features and capabilities of Angular JavaScript Library by Google',
+};
+
+})();
