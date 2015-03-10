08533f71038437d623a0d584a2bab020f408ba48
VictoriaKio <victoria.cj.kio@gmail.com>
2015-03-10 22:46:21 +0000

    Array of objects, use of ng-repeat, and ng-src
    On the README page, the text, and images on the sub content sections
    are now encapsulated in an array of objects in the app.js file, this
    was then called via the controller with alias mainWiki.
    
    I also experimented with two angular directives ng-repeat, and ng-src

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-//Creating a Module - Test
-(function(){
-var app = angular.module("test", [ ]);
+//Creating a Module - project1
+(function()
+{
+var app = angular.module("project1", [ ]);
 
 app.controller('WikiText', function(){
 
 	this.wikicontent=wikicontent;
+	this.wikiSubcontents = wikiSubcontents;
 
 });
+
 //Wiki content object
 //Need more objects, create an array of objects and use ng-repeat to display
 var wikicontent = {
@@ -14,5 +17,24 @@ var wikicontent = {
 	title: 'Project Sandbox Angular',
 	description: 'These webpages are to study the features and capabilities of Angular JavaScript Library by Google',
 };
+
+var wikiSubcontents = [
+{title: 'Project 1',
+paragraph: 'Project 1, at the moment looks like it is going to be the creation of this website where I am hopping to predominately create this with Angular, but I there also will be the need to understand more about Bootstrap.',
+moreText:'If there where more details they will be accessible via this link.',
+image:'images/space1.jpeg'},
+
+{title:'Project X',
+paragraph:'Lets complete Project 1 first.',
+moreText:'Under construction',
+image:'images/space2.jpeg'},
+
+{title:'Project X',
+paragraph: 'Ditto Project X, and we will see how it goes.',
+moreText:'Under construction',
+image:'images/space3.jpeg'}
+
+];
+
 
 })();
